519ce6456b859293123ad034687c35c6656add91
cashchappo <107930591+cashchappo@users.noreply.github.com>
2022-08-16 19:36:39 +0400

    Create 2.swift

---
+++ 2.swift
@@ -0,0 +1,94 @@
+//
+//  ResultViewController.swift
+//  NamesCompApp
+//
+//  Created by Danil Bochkarev on 16.08.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var namesLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var firstName: String!
+    var secondName: String!
+    
+    private var resultValue = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //вызываем функцию для подсчета
+        resultValue = findResult()
+
+        namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
+        resultLabel.text = resultValue.formatted(.percent)
+        
+        progressView.progress = Float(resultValue) / 100
+        
+        //изменяем размер прогресса
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 3)
+        
+    }
+    
+    //в зависимости от буквы добавляется значение к count
+    private func findValue (for name: String) -> Int {
+        var count = 0
+        
+        for character in name.lowercased() {
+            switch character {
+            case "a", "i", "j", "q", "y":
+                count += 1
+            case "b", "k", "r":
+                count += 2
+            case "c", "g", "l", "s":
+                count += 3
+            case "d", "m", "t":
+                count += 4
+            case "e", "h", "n", "x":
+                count += 5
+            case "u", "v", "w":
+                count += 6
+            case "o", "z":
+                count += 7
+            case "f", "p":
+                count += 8
+            default:
+                count += 0
+            }
+        }
+        return count
+    }
+    
+    //потом проверяется результат после функции findValue
+    private func findResult() -> Int {
+        var result = 0
+        
+        let firstValue = findValue(for: firstName)
+        let secondValue = findValue(for: secondName)
+        let absDifference = abs(firstValue - secondValue)
+        
+        switch absDifference {
+        case 0, 1, 2:
+            result = 100
+        case 3, 4:
+            result = 65
+        case 5:
+            result = 50
+        case 6:
+            result = 40
+        default:
+            result = 30
+        }
+        return result
+    }
+    
+    
+}
